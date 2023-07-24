cce990799ca20ff365f948dbaa4f54baf6331628
Jovan <jovanpavlovic2018@gmail.com>
2023-07-24 12:20:08 +0200

    added styles,animation,tidying up code

--- javascript.js
+++ javascript.js
@@ -10,51 +10,48 @@ function getComputerChoice() {
 
 function rockPaperScissors(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("It's same choice!");     
+        msg.textContent = 'SAME CHOICE,PLAY AGAIN!'
     }
     else if ((playerSelection === 'rock' && computerSelection === 'paper') ||
         (playerSelection === 'paper' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'rock')) {
-        console.log(`COMPUTER WINS! ${computerSelection} beats ${playerSelection}`);
         computerWins = computerWins + 1;
         computerRes.textContent = computerWins;
-        console.log(`Current result : You ${playerWins} - ${computerWins} Machine`);
+
+        text = `<span style='color:red'>${computerSelection}</span> BEATS <span style='color:green'>${playerSelection}</span> YOU LOST`;
+        msg.innerHTML = text;
     }
     else {
-        console.log(`YOU WIN! ${playerSelection} beats ${computerSelection}`);
         playerWins = playerWins + 1;
         playerRes.textContent = playerWins;
-        console.log(`Current result : You ${playerWins} - ${computerWins} Machine`);
+        text = `<span style='color:green'>${playerSelection}</span> BEATS <span style='color:red'>${computerSelection}</span> YOU WON`;
+        msg.innerHTML = text;
     }
 }
 
 
 function game() {
     computerSelection = getComputerChoice();
-    if (this.className === computerSelection) {
-        msg2.textContent = 'SAME CHOICE PLAY AGAIN!';
-    }
-    else {
-        msg2.textContent = '';
-    }
     if (this.className === 'rock') {
         playerSelection = 'rock';
-        playerChoiceImage.src = '../rock-paper-scissors/pictures/rock.png'; 
+        this.classList.add('played');
+        playerChoiceImage.src = '../rock-paper-scissors/pictures/rock.png';
         computerChoiceImage.src = `../rock-paper-scissors/pictures/${computerSelection}.png`;
     }
     else if (this.className === 'paper') {
         playerSelection = 'paper';
-        playerChoiceImage.src = '../rock-paper-scissors/pictures/paper.png'; 
+        this.classList.add('played');
+        playerChoiceImage.src = '../rock-paper-scissors/pictures/paper.png';
         computerChoiceImage.src = `../rock-paper-scissors/pictures/${computerSelection}.png`;
     }
     else {
         playerSelection = 'scissors';
-        playerChoiceImage.src = '../rock-paper-scissors/pictures/scissors.png'; 
+        this.classList.add('played');
+        playerChoiceImage.src = '../rock-paper-scissors/pictures/scissors.png';
         computerChoiceImage.src = `../rock-paper-scissors/pictures/${computerSelection}.png`;
     }
     rockPaperScissors(playerSelection, computerSelection);
-    
-    msg.textContent = `YOU ${playerWins} - ${computerWins} MACHINE`;
+
     if (playerWins === 5) {
         msg.textContent = 'YOU SAVED HUMANITY';
         main.style.display = 'none';
@@ -66,6 +63,14 @@ function game() {
         reset.style.display = 'block';
     }
 }
+
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('played');
+}
+
+
 const playerRes = document.querySelector('#playerRes');
 const computerRes = document.querySelector('#computerRes');
 computerRes.textContent = 0;
@@ -74,17 +79,17 @@ const playerChoiceImage = document.querySelector('#player');
 const computerChoiceImage = document.querySelector('#computer');
 const main = document.querySelector('.main');
 const msg = document.querySelector('.msg');
-const msg2 = document.querySelector('.msg2');
 const reset = document.querySelector('#reset');
-const secondary = document.querySelector('.secondary');
 const images = document.querySelectorAll('img:not(#player):not(#computer)');
 
 
-images.forEach(img => img.addEventListener('click',game));
+images.forEach(img => img.addEventListener('click', game));
+images.forEach(img => img.addEventListener('transitionend', removeTransition));
 
 
 
 reset.addEventListener('click', () => {
+    images.forEach(img => { img.classList.remove('played') });
     playerWins = 0;
     computerWins = 0;
     computerRes.textContent = 0;
